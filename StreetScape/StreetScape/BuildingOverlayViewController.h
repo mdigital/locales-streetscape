bb41fd62335800506844460ec5a19d429d023174
Robin Marshall <robin@mdigital.co.nz>
2013-02-26 07:25:04 +1300

    Basic overlay view controller

---
+++ StreetScape/StreetScape/BuildingOverlayViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuildingOverlayViewController.h
+//  StreetScape
+//
+//  Created by Robin Marshall on 12/01/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuildingOverlayViewController : UIViewController
+
+@end
---
+++ StreetScape/StreetScape/BuildingOverlayViewController.m
@@ -0,0 +1,38 @@
+//
+//  BuildingOverlayViewController.m
+//  StreetScape
+//
+//  Created by Robin Marshall on 12/01/13.
+//
+//
+
+#import "BuildingOverlayViewController.h"
+
+@interface BuildingOverlayViewController ()
+
+@end
+
+@implementation BuildingOverlayViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
